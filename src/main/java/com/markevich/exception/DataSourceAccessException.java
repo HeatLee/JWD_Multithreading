c7319c2ecb2eb711dae7dec850ecfa215daa5561
HeatLee <unheatlee@gmail.com>
2020-02-15 12:33:57 +0300

    Add data access exception

---
+++ src/main/java/com/markevich/exception/DataSourceAccessException.java
@@ -0,0 +1,19 @@
+package com.markevich.exception;
+
+public class DataSourceAccessException extends Exception {
+    public DataSourceAccessException() {
+        super();
+    }
+
+    public DataSourceAccessException(String message) {
+        super(message);
+    }
+
+    public DataSourceAccessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DataSourceAccessException(Throwable cause) {
+        super(cause);
+    }
+}
